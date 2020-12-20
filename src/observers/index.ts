27f3fd4fc6898dad98ff5d2767ff6baeea57c611
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-12-20 14:16:57 +0000

    Deploy defective-origin/html-dom-navigator to github.com/defective-origin/html-dom-navigator.git:gh-pages

--- src/observers/index.ts
+++
@@ -1,9 +0,0 @@
-import NavTreeClickObserver from './NavTreeClickObserver'
-import NavTreeHtmlObserver from './NavTreeHtmlObserver'
-import NavTreeKeyPressObserver from './NavTreeKeyPressObserver'
-
-export default {
-  NavTreeClickObserver,
-  NavTreeHtmlObserver,
-  NavTreeKeyPressObserver,
-}
